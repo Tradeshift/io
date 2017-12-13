bcb0eaa9e90c805f64f409abed6ef718070987bf
Daniel Spitzer <dsp@tradeshift.com>
2017-12-13 12:48:06 +0100

    [build] Init rollup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default [
+	{
+		input: 'src/main.js',
+		name: 'ts.app',
+		sourcemap: true,
+		output: [
+			{ file: pkg.browser, format: 'umd' },
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		],
+		plugins: [
+			babel({
+				exclude: 'node_modules/**'
+			})
+		]
+	}
+];
