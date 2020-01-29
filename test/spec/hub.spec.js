974a540cdd55f13cdc5341481135cef16314a3c0
Jacob Wejendorp <jwe@tradeshift.com>
2020-01-29 11:39:07 +0100

    Rewrite spec for clarity and remove try/catch

--- test/spec/hub.spec.js
+++ test/spec/hub.spec.js
@@ -1,53 +1,64 @@
-import {
-	verifyTestMessage,
-	removeAppOrFail,
-	inBrowserstack
-} from '../lib/helpers';
+import { verifyTestMessage, removeAppOrFail } from '../lib/helpers';
 
 export default ({ apps, hub }) => {
-	it('hub > handleAppTimeout', done => {
-		let connectCount = 0;
-		const connectListener = event => {
-			const testMessage = event.data;
-			if (!verifyTestMessage(testMessage, event.source)) {
-				return;
+	const waitForTimeout = () =>
+		new Promise(resolve => {
+			hub._fakeTimeout = resolve;
+		});
+
+	describe('hub', () => {
+		let onMessage;
+		const eventListener = evt => {
+			if (onMessage) {
+				onMessage(evt);
 			}
-			if (testMessage.io.method === 'connect' && ++connectCount === 4) {
-				const frames = window.document.getElementsByTagName('iframe');
-				expect(frames.length).toEqual(4);
-				for (let i = frames.length - 1; i >= 0; i--) {
-					const frame = frames[i];
-					try {
-						if (frame && frame.parentNode) {
-							frame.parentNode.removeChild(frame);
-						}
-					} catch (e) {
-						console.log(
-							'[hub > handleAppTimeout] removeChild failed.\n' +
-								JSON.stringify(e, null, 2)
-						);
+		};
+		const waitForMethod = method =>
+			new Promise(resolve => {
+				onMessage = event => {
+					const testMessage = event.data;
+					if (!verifyTestMessage(testMessage, event.source)) {
+						return;
 					}
-				}
-				window.removeEventListener('message', connectListener);
-			}
-		};
-		window.addEventListener('message', connectListener);
+					if (testMessage.io.method === method) {
+						resolve();
+					}
+				};
+			});
 
-		let timeoutCount = 0;
-		hub._fakeTimeout = (app, win) => {
-			removeAppOrFail(
-				app,
-				apps,
-				'App timeout() called more than once for the same app!'
-			);
+		beforeEach(() => {
+			window.addEventListener('message', eventListener);
+		});
+		afterEach(() => {
+			window.removeEventListener('message', eventListener);
+		});
 
-			if (++timeoutCount === 4) {
-				hub._fakeTimeout = () => {};
-				if (!inBrowserstack()) {
-					console.log('DONE - hub > handleAppTimeout');
+		it('#handleAppTimeout gets called for all 4 apps', async done => {
+			// Await frames
+			await waitForMethod('connect');
+			await waitForMethod('connect');
+			await waitForMethod('connect');
+			await waitForMethod('connect');
+
+			const frames = window.document.getElementsByTagName('iframe');
+			expect(frames.length).toEqual(4);
+			// remove frames to force timeout
+			for (let i = frames.length - 1; i >= 0; i--) {
+				const frame = frames[i];
+				if (frame && frame.parentNode) {
+					frame.parentNode.removeChild(frame);
 				}
-				done();
 			}
-		};
+
+			for (let i = 0; i < 4; i++) {
+				const app = await waitForTimeout();
+				removeAppOrFail(
+					app,
+					apps,
+					'App timeout() called more than once for the same app!'
+				);
+			}
+			done();
+		}, 5000);
 	});
 };
